d760238091da2c8b8aa7670cd7136288a4cfa981
pohaxostore <pohaxostore@gmail.com>
2026-06-28 14:05:13 +0100

    Add back to top button functionality
    Implement back to top button functionality with scroll event listener.

--- js/script.js
+++ js/script.js
@@ -114,3 +114,34 @@ window.addEventListener("scroll", () => {
     progressBar.style.width = progress + "%";
 
 });
+/*=========================================
+    Back To Top
+=========================================*/
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 500){
+
+        backToTop.classList.add("show");
+
+    }else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
